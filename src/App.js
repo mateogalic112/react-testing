d20cce799ea3427d9ef5d7f95cae878b64d93474
Mateo Galic <mateogalic112@gmail.com>
2021-05-12 23:42:53 +0200

    Adding checkbox dependency.

--- src/App.js
+++ src/App.js
@@ -3,13 +3,24 @@ import "./App.css";
 
 function App() {
   const [buttonColor, setButtonColor] = React.useState("red");
+  const [checked, setChecked] = React.useState(false);
   const buttonColorText = buttonColor === "red" ? "blue" : "red";
+
   return (
     <div>
       <button
         style={{ backgroundColor: buttonColor }}
         onClick={() => setButtonColor(buttonColorText)}
+        disabled={checked}
       >{`Change to ${buttonColorText}`}</button>
+
+      <input
+        type="checkbox"
+        id="enable-button-checkbox"
+        aria-checked={checked}
+        value={checked}
+        onChange={(e) => setChecked(e.target.checked)}
+      />
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -11,3 +11,26 @@ test("button has correct initial color", () => {
   expect(colorButton).toHaveStyle({ backgroundColor: "blue" });
   expect(colorButton.textContent).toBe("Change to red");
 });
+
+test("initial conditions", () => {
+  render(<App />);
+
+  const button = screen.getByRole("button", { name: "Change to blue" });
+  expect(button).toBeEnabled();
+
+  const checkbox = screen.getByRole("checkbox");
+  expect(checkbox).not.toBeChecked();
+});
+
+test("checking and unchecking checkbox", () => {
+  render(<App />);
+
+  const checkbox = screen.getByRole("checkbox");
+  const button = screen.getByRole("button", { name: "Change to blue" });
+
+  fireEvent.click(checkbox);
+  expect(button).toBeDisabled();
+
+  fireEvent.click(checkbox);
+  expect(button).toBeEnabled();
+});
