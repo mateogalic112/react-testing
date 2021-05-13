b0910b9fd0257b38aaf485aa54da26205d102907
Mateo Galic <mateogalic112@gmail.com>
2021-05-13 10:05:12 +0200

    Adding eslint and prettier settings.

--- src/App.test.js
+++ src/App.test.js
@@ -13,7 +13,7 @@ test("button has correct initial color", () => {
 
   fireEvent.click(colorButton);
   expect(colorButton).toHaveStyle({ backgroundColor: "MidnightBlue" });
-  expect(colorButton.textContent).toBe("Change to Medium Violet Red");
+  expect(colorButton).toHaveTextContent("Change to Medium Violet Red");
 });
 
 test("initial conditions", () => {
